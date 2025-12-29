eaeb41d55d591f288391a7c0084f0b2622c46ef0
kkuu22113 <77877325+imxuebi@users.noreply.github.com>
2025-12-29 20:28:54 +0800

    Update hf_model.py
    feat(model): add HFAutoModel for generic model loading" -m "- Adds HFAutoModel class to support loading models using AutoModel.from_pretrained, enabling support for multimodal architectures.

--- python/fate_llm/model_zoo/hf_model.py
+++ python/fate_llm/model_zoo/hf_model.py
@@ -14,7 +14,7 @@
 #  limitations under the License.
 #
 import torch
-from transformers import AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, AutoModel
 
 
 class HFAutoModelForCausalLM:
@@ -29,6 +29,23 @@ class HFAutoModelForCausalLM:
 
     def load(self):
         model = AutoModelForCausalLM.from_pretrained(
+            self.pretrained_model_name_or_path, *self.model_args, **self.kwargs
+        )
+        return model
+
+
+class HFAutoModel:
+
+    def __init__(self, pretrained_model_name_or_path, *model_args, **kwargs) -> None:
+        self.pretrained_model_name_or_path = pretrained_model_name_or_path
+        self.model_args = model_args
+        self.kwargs = kwargs
+        if "torch_dtype" in self.kwargs and self.kwargs["torch_dtype"] != "auto":
+            dtype = self.kwargs.pop("torch_dtype")
+            self.kwargs["torch_dtype"] = getattr(torch, dtype)
+
+    def load(self):
+        model = AutoModel.from_pretrained(
             self.pretrained_model_name_or_path, *self.model_args, **self.kwargs
         )
         return model
